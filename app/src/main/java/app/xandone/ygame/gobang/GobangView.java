d0e0288e12a839512b3a92e78a9e4b5ce6e7beeb
xandone <765478955@qq.com>
2017-09-16 00:01:55 +0800

    init

--- app/src/main/java/app/xandone/ygame/gobang/GobangView.java
+++ app/src/main/java/app/xandone/ygame/gobang/GobangView.java
@@ -2,13 +2,19 @@ package app.xandone.ygame.gobang;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
+import app.xandone.ygame.R;
 import app.xandone.ygame.Utils;
 
 /**
@@ -20,16 +26,21 @@ public class GobangView extends View {
     private Context mContext;
     private Paint mPaintBg;
     private Paint mPaint;
-
-    private int mWidth, mHeight;
-
     private Bitmap mBitmapBg;
     private Canvas mCavasBuffer;
 
+    private int mWidth, mHeight;
+    private int mDefaultSize;
+    private int mPieceSize;
+
+    private Piece mWhitePiece;
+    private Piece mBlackPiece;
+    private Bitmap mWhiteBtm;
+    private Bitmap mBlackBtm;
+    private boolean isWhite;
+
     public static final int MAX_LINE_W = 8;
     public static final int MAX_LINE_H = 10;
-
-    private int mDefaultSize;
 
     public GobangView(Context context) {
         this(context, null);
@@ -56,31 +67,37 @@ public class GobangView extends View {
 
         mDefaultSize = Utils.dp2px(mContext, 100);
 
+        mWhiteBtm = BitmapFactory.decodeResource(getResources(), R.drawable.p2);
+        mBlackBtm = BitmapFactory.decodeResource(getResources(), R.drawable.p1);
+
+        isWhite = true;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawBitmap(mBitmapBg, 0, 0, mPaint);
+        mWhitePiece.drawAll(mPaint, canvas);
+        mBlackPiece.drawAll(mPaint, canvas);
     }
 
     public void drawbgBuffer(Canvas canvas) {
         mPaint.setColor(Color.BLACK);
-        int wid = mWidth / MAX_LINE_W;
+        mPieceSize = mWidth / MAX_LINE_W;
         int startX = 0;
         int startY = 0;
-        int endX = wid * MAX_LINE_W;
+        int endX = mPieceSize * MAX_LINE_W;
         int endY = 0;
         for (int i = 0; i <= MAX_LINE_H; i++) {
-            startY = i * wid;
-            endY = i * wid;
+            startY = i * mPieceSize;
+            endY = i * mPieceSize;
             canvas.drawLine(startX, startY, endX, endY, mPaint);
         }
         startY = 0;
-        endY = wid * MAX_LINE_H;
+        endY = mPieceSize * MAX_LINE_H;
         for (int i = 0; i <= MAX_LINE_W; i++) {
-            startX = i * wid;
-            endX = i * wid;
+            startX = i * mPieceSize;
+            endX = i * mPieceSize;
             canvas.drawLine(startX, startY, endX, endY, mPaint);
         }
     }
@@ -122,5 +139,41 @@ public class GobangView extends View {
         mBitmapBg = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
         mCavasBuffer = new Canvas(mBitmapBg);
         drawbgBuffer(mCavasBuffer);
+
+        mWhitePiece = new Piece(Bitmap.createScaledBitmap(mWhiteBtm, mPieceSize, mPieceSize, false));
+        mBlackPiece = new Piece(Bitmap.createScaledBitmap(mBlackBtm, mPieceSize, mPieceSize, false));
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                int x = (int) event.getX();
+                int y = (int) event.getY();
+                int point_x = x / mPieceSize * mPieceSize - mPieceSize / 2;
+                int point_y = y / mPieceSize * mPieceSize - mPieceSize / 2;
+                Point point = new Point(point_x, point_y);
+                if (isWhite) {
+                    if (mWhitePiece.contains(point)) {
+                        return true;
+                    }
+                    mWhitePiece.addPonit(point);
+                } else {
+                    if (mBlackPiece.contains(point)) {
+                        return true;
+                    }
+                    mBlackPiece.addPonit(point);
+                }
+                isWhite = !isWhite;
+                invalidate();
+                break;
+        }
+
+        return true;
     }
 }
